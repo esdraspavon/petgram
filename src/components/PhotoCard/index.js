74fd75cd42c3caa2334fb0505d6f7e505584ce51
Esdras Pavon <pavonesdras@gmail.com>
2019-07-30 13:00:40 -0300

    Agregado graphql y obteniendo foto

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -7,7 +7,7 @@ import { useNearScreen } from '../../hooks/useNearScreen'
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const [show, element] = useNearScreen
+  const [show, element] = useNearScreen()
 
   const key = `like-${id}`
   const [liked, setLiked] = useLocalStorage(key, false)
