e960928c0ed622859b3b128f57a7f02700966516
Esdras Pavon <pavonesdras@gmail.com>
2019-07-31 21:44:47 -0300

    Mostrando foto individual

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,37 +1,35 @@
-import React, { Fragment, useEffect, useRef, useState } from 'react'
-import { ImgWrapper, Img, Button, Article } from './styles'
-import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
-import { useLocalStorage } from '../../hooks/useLocalStorage'
-import { useNearScreen } from '../../hooks/useNearScreen'
+import React, { Fragment, useEffect, useRef, useState } from "react";
+import { ImgWrapper, Img, Button, Article } from "./styles";
+import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
+import { useLocalStorage } from "../../hooks/useLocalStorage";
+import { useNearScreen } from "../../hooks/useNearScreen";
 
-const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
+const DEFAULT_IMAGE =
+  "https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png";
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const [show, element] = useNearScreen()
+  const [show, element] = useNearScreen();
 
-  const key = `like-${id}`
-  const [liked, setLiked] = useLocalStorage(key, false)
+  const key = `like-${id}`;
+  const [liked, setLiked] = useLocalStorage(key, false);
 
-  const Icon = liked ? MdFavorite : MdFavoriteBorder
-
+  const Icon = liked ? MdFavorite : MdFavoriteBorder;
   return (
     <Article ref={element}>
-      {
-        show &&
+      {show && (
         <Fragment>
-          <a href={`/detail/${id}`}>
+          <a href={`/?detail=${id}`}>
             <ImgWrapper>
               <Img src={src} />
             </ImgWrapper>
           </a>
 
           <Button onClick={() => setLiked(!liked)}>
-            <Icon size='32px' />{likes} likes!
+            <Icon size="32px" />
+            {likes} likes!
           </Button>
         </Fragment>
-
-      }
-
+      )}
     </Article>
-  )
-}
+  );
+};
