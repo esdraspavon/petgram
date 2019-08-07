2e26990d23f3802bf196f6fb5b9436f251b308d3
Esdras Pavon <pavonesdras@gmail.com>
2019-08-06 21:21:51 -0300

    Añadiendo react Helmet, creado pagina de 404

--- src/App.js
+++ src/App.js
@@ -1,41 +1,36 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 import { Home } from './pages/Home'
 import { NavBar } from './components/NavBar'
 
-import { Router } from '@reach/router'
+import { Router, Redirect } from '@reach/router'
 
 import { Detail } from './pages/Detail'
 import { Favs } from './pages/Favs'
 import { User } from './pages/User'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
-import Context from './Context'
+import { NotFound } from './pages/NotFound'
+import { Context } from './Context'
 
 export const App = () => {
+  const { isAuth } = useContext(Context)
   return (
     <div>
       <GlobalStyle />
       <Logo />
       <Router>
+        <NotFound default />
         <Home path='/' />
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
+        {!isAuth && <NotRegisteredUser path='/login' />}
+        {!isAuth && <Redirect from='/favs' to='login' />}
+        {!isAuth && <Redirect from='/user' to='login' />}
+        {isAuth && <Redirect from='/login' to='/' />}
+        <Favs path='/favs' />
+        <User path='/user' />
       </Router>
-      <Context.Consumer>
-        {
-          ({ isAuth }) =>
-            isAuth
-              ? <Router>
-                <Favs path='/favs' />
-                <User path='/user' />
-              </Router>
-              : <Router>
-                <NotRegisteredUser path='/favs' />
-                <NotRegisteredUser path='/user' />
-              </Router>
-        }
-      </Context.Consumer>
       <NavBar />
     </div>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,13 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { ListOfPhotoCards } from '../container/ListOfPhotoCards'
 import { ListOfCategories } from '../components/ListOfCategories'
+import { Layout } from '../components/Layout'
 
 export const Home = ({ id }) => {
   return (
-    <Fragment>
+    <Layout title='Tu app de fotos de mascotas' subtitle='Con Petgram puedes encontrar fotos de animales dómesticos muy bonitos'>
       <ListOfCategories />
       <ListOfPhotoCards categoryId={id} />
-    </Fragment>
+    </Layout>
   )
 }
