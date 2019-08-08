48f9d4371a2cd526fa4069ad453eee3acad8a016
Esdras Pavon <pavonesdras@gmail.com>
2019-08-07 21:27:08 -0300

    Agregando react lazy, y prototypes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, Suspense } from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 import { Home } from './pages/Home'
@@ -7,16 +7,20 @@ import { NavBar } from './components/NavBar'
 import { Router, Redirect } from '@reach/router'
 
 import { Detail } from './pages/Detail'
-import { Favs } from './pages/Favs'
+
+// import { Favs } from './pages/Favs'
+
 import { User } from './pages/User'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
 import { NotFound } from './pages/NotFound'
 import { Context } from './Context'
 
+const Favs = React.lazy(() => import('./pages/Favs'))
+
 export const App = () => {
   const { isAuth } = useContext(Context)
   return (
-    <div>
+    <Suspense fallback={<div />}>
       <GlobalStyle />
       <Logo />
       <Router>
@@ -32,6 +36,6 @@ export const App = () => {
         <User path='/user' />
       </Router>
       <NavBar />
-    </div>
+    </Suspense>
   )
 }
